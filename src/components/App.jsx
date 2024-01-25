d5b7fb668e2d42fa2518b2cf2aca33cd27dcd470
Jon <128969752+jonp-h@users.noreply.github.com>
2024-01-25 12:52:05 +0100

    Add Card component and CardData module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import "../App.css";
+import Card from "./Card";
 import TekstBoks from "./TekstBoks";
 
 function App() {
   return (
-    <div className=" h-screen bg-slate-800">
-      <h1 className=" text-white">Hello </h1>
+    <div className=" grid grid-cols-3 pt-5 h-screen bg-slate-800">
+      {/* <h1 className=" text-white">Hello </h1>
       <p className="text-white">Dette er en paragraf</p>
-      <TekstBoks />
+      <h2 className=" text-blue-400">Dette er en test</h2> */}
+      {/* <TekstBoks /> */}
+      <Card name="Benjamin Button" text="Lorem ipsum.." online="true" />
+      <Card name="Shroud" text="Veldig flink på dataspill." online="true" />
+      <Card name="Ninja" text="Ikke spesielt populær lengre." online="true" />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className=" mx-3 w-fit bg-white rounded-lg p-2 flex flex-col">
+      <img
+        className=" max-w-40 rounded-xl"
+        src="https://source.unsplash.com/random/300x400/?people"
+      />
+      <div className="p-2 bg-white">
+        <h3 className="mb-1 font-bold">{props.name}</h3>
+        <p className=" w-36"> {props.text}</p>
+      </div>
+    </div>
+  );
+}
